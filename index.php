1ed49d5603a9970c240edbc1735b2e8173256703
Alex Bunting <alex.bunting@gmail.com>
2024-09-02 16:57:19 +0100

    commit

---
+++ index.php
@@ -0,0 +1,262 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alex Bunting - A Web Developer</title>
+    <link rel="icon" type="image/x-icon" href="./images/ICON/icon.ico">
+
+    <link rel="stylesheet" type="text/css" href="css/main.css" />
+
+    <link rel="preconnect" href="https://fonts.googleapis.com" crossorigin>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Jaro:opsz@6..72&family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="preload" as="font" type="font/woff2" crossorigin>
+    <!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Jaro:opsz@6..72&family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap"> -->
+
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script> -->
+    <script src="./javascript/jqmin.js"></script>
+</head>
+
+<body class="retro-theme">
+
+    <div class="makeItLarge">
+        <div class="makeItLarge__background"></div>
+        <div class="makeItLarge__image">
+            <img class="enlargedImage" src="" alt="Its a placeholder...">
+        </div>
+    </div>
+
+    <div class="pageLoadSfx__name jazzyText">
+        Alex Bunting
+    </div>
+
+    <div class="pageLoadSfx">
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+        <div class="colorSwipe__bar"></div>
+    </div>
+
+    <div class="structure">
+
+        <label class="structure__hamburger" for="hamburgerOpen">
+            <div class="structure__hamburger--line"></div>
+        </label>
+        <input type="checkbox" class="structure__hamburger--button" name="hamburgerOpen" id="hamburgerOpen" value="2">
+
+        <div class="structure__hamburger--menu">
+            <div class="structure__hamburger--backdrop"></div>
+            <div class="structure__hamburger--top">
+                <div class="structure__hamburger--name jazzyText">
+                    <div>Alex Bunting</div>
+                </div>
+
+                <div class="structure__line"></div>
+
+                <div class="structure__hamburger--buttons">
+                    <div class="structure__button" data-pointer="main.php">TL:DR</div>
+                    <div class="structure__button" data-pointer="uiux.php?project=0">Front End Development</div>
+                    <div class="structure__button" data-pointer="coding.php">Technical Skills</div>
+                    <div class="structure__button" data-pointer="soft.php">Soft Skills</div>
+                </div>
+
+                <div class="structure__accessibility">
+                    <img src="./images/ICON/001-sign.png" alt="Retro" class="switchColourScheme" data-mode="retro-theme">
+                    <img src="./images/ICON/002-night-mode.png" alt="Dark" class="switchColourScheme" data-mode="goth-mode">
+                </div>
+            </div>
+
+        </div>
+
+        <div class="structure__menu">
+            <div class="structure__name jazzyText">
+                <div class="structure__name--name">Alex</div>
+                <div class="structure__name--name">Bunting</div>
+            </div>
+
+            <div class="structure__line"></div>
+
+            <div class="structure__dtwo">A full stack developer creating exceptional Angular frontend and NodeJS backend software.</div>
+
+            <div class="structure__line"></div>
+
+            <div class="structure__buttons">
+                <div class="structure__button" data-pointer="main.php">
+                    <p>TL:DR</p>
+                </div>
+                <div class="structure__button" data-pointer="uiux.php?project=0">
+                    <p>Portfolio</p>
+                </div>
+                <div class="structure__button" data-pointer="coding.php">
+                    <p>Technical Skills</p>
+                </div>
+                <div class="structure__button" data-pointer="soft.php">
+                    <p>Soft Skills</p>
+                </div>
+            </div>
+
+            <div class="structure__accessibility">
+                <!-- <div class="structure__accessibility--box structure__accessibility--theme structure__accessibility--green">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div class="structure__accessibility--box structure__accessibility--theme structure__accessibility--retro">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div> -->
+                <img src="./images/ICON/001-sign.png" alt="Retro" class="switchColourScheme" data-mode="retro-theme">
+                <img src="./images/ICON/002-night-mode.png" alt="Dark" class="switchColourScheme" data-mode="goth-mode">
+                <!-- <div class="structure__accessibility--box structure__accessibility--font">
+                    <input type="checkbox" name="font" id="font">
+                    <label for="font" class="structure__accessibility--font--icon">
+                        A
+                    </label>
+
+                    <div class="structure__accessibility--font--display">
+                        <label for="font">X</label>
+                        <input type="range" min="50" max="125" name="fontSize" id="fontSize" class="structure__accessibility--slider">
+                    </div>
+                </div> -->
+            </div>
+
+        </div>
+
+        <div id="load" class="structure__load">
+            <div class="loader">
+                <div class="loader__background"></div>
+                <div class="loader__front">
+                    <div class="loader__spinner"></div>
+                </div>
+            </div>
+
+            <div id="load__content" class="structure__load--content"></div>
+        </div>
+    </div>
+
+    <script src="./javascript/js.js"></script>
+
+</body>
+
+</html>
---
+++ pages/main.php
@@ -0,0 +1,220 @@
+<div class="page">
+    <div class="intro">
+        <div class="intro__top">
+            <div class="intro__top--banner">
+
+            </div>
+            <div class="intro__top--tldr">TL:DR</div>
+            <div class="intro__top--right">
+                <div class="intro__top--me retro-theme">
+                    <img src="./images/potentialme/alex-fancy-cropped.jpg" alt="Me">
+                </div>
+                <div class="intro__top--me goth-mode">
+                    <img src="./images/potentialme/alex-big.jpg" alt="Me">
+                </div>
+                <div class="intro__top--buttons">
+                    <a href="https://www.linkedin.com/in/a-bunting" class="intro__top--button" target="_blank">
+                        <img src="./images/ICON/LI-In-Bug.png" alt="LinkedIn">
+                    </a>
+                    <a href="https://github.com/a-bunting" class="intro__top--button" target="_blank">
+                        <img src="./images/ICON/Git-Icon-1788C.png" alt="GIT">
+                    </a>
+                    <a href="./CV/CV - Alex Bunting.pdf" class="intro__top--button" target="_blank">
+                        <img src="./images/ICON/profile.svg" alt="CV">
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="intro__projects--text">
+            A <strong>statement</strong> from myself
+        </div>
+
+        <div class="intro__statement">
+            <p>
+                I am a full stack web developer creating user focussed, complex software solutions. I primarily work with an Angular frontend, NodeJS backend and MySQL database. I love building web sites and the more challenging, the better!
+            </p>
+        </div>
+
+        <div class="intro__title">
+            <p><strong>Web Developer</strong> / Software Engineer</p>
+        </div>
+
+        <div class="intro__tech">
+            <div class="intro__tech--text">
+                <p>Tech</p>
+                <p>Stack</p>
+            </div>
+            <div class="intro__techlist">
+                <div class="intro__techlist--row">
+                    <img class="intro__techlist--short" src="./images/logo/Angular_trimmed.svg" alt="Angular">
+                    <img class="intro__techlist--short" src="./images/logo/nodejs.png" alt="NodeJS">
+                    <img class="intro__techlist--long" src="./images/logo/mysql.png" alt="MySQL">
+                    <img class="intro__techlist--short" src="./images/logo/php.png" alt="PHP">
+                    <img class="intro__techlist--short" src="./images/logo/javascript.png" alt="Javascript">
+                    <img class="intro__techlist--short" src="./images/logo/CSS3_logo_and_wordmark.svg" alt="CSS3">
+                    <img class="intro__techlist--long" src="./images/logo/sass.png" alt="SASS">
+                    <img class="intro__techlist--short" src="./images/logo/HTML_Logo2.svg" alt="HTML5">
+                    <img class="intro__techlist--long" src="./images/logo/jQuery-Logo.png" alt="jQuery">
+                    <img class="intro__techlist--long" src="./images/logo/mongodb.svg" alt="MongoDB">
+                </div>
+                <!-- <div class="intro__techlist--row">
+                </div> -->
+            </div>
+        </div>
+
+        <div class="intro__projects--text">Selected <strong>Projects</strong></div>
+
+        <div class="intro__projects">
+            <div class="intro__project">
+                <div class="intro__project--inner">
+                    <!-- frnt is for an image, desc and name -->
+                    <div class="intro__project--front">
+                        <div class="intro__project--image">
+                            <img src="./images/smartsort-big.jpg" alt="Smartsort" class="intro__project--background">
+                            <img src="./images/smartsort-logo.png" alt="Smartsort Logo" class="intro__project--logo">
+                        </div>
+                    </div>
+                    <!-- back is for tech and links -->
+                    <div class="intro__project--back">
+                        <div class="intro__project--back--one">
+                            <div class="intro__project--tech">
+                                <img class="intro__project--short" src="./images/logo/Angular_trimmed.svg" alt="Angular">
+                                <img class="intro__project--short" src="./images/logo/nodejs.png" alt="NodeJS">
+                                <img class="intro__project--long" src="./images/logo/mysql.png" alt="MySQL">
+                                <img class="intro__project--long" src="./images/logo/sass.png" alt="SASS">
+                                <img class="intro__project--short" src="./images/logo/HTML_Logo2.svg" alt="HTML5">
+                            </div>
+                        </div>
+                        <div class="intro__project--back--one">
+                            <div class="intro__project--links">
+                                <a href="https://www.smartsort.org/" class="intro__top--button" target="_blank">
+                                    <img src="./images/logo/liveicon.png" alt="Live Site">
+                                </a>
+                                <a href="https://github.com/a-bunting/class-organiser" class="intro__top--button" target="_blank">
+                                    <img src="./images/ICON/Git-Icon-1788C.png" alt="GIT">
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="intro__project--floater">
+                    <p>Smartsort is a lightweight scheduling system designed to easily allow schools to design student centered learning areas.</p>
+                </div>
+            </div>
+
+
+            <div class="intro__project">
+                <div class="intro__project--inner">
+                    <!-- frnt is for an image, desc and name -->
+                    <div class="intro__project--front">
+                        <div class="intro__project--image">
+                            <img src="./images/rz1.jpg" alt="ReportsZone" class="intro__project--background">
+                            <img src="./images/ReportZone.png" alt="ReportsZone Logo" class="intro__project--logo">
+                        </div>
+                    </div>
+                    <!-- back is for tech and links -->
+                    <div class="intro__project--back">
+                        <div class="intro__project--back--one">
+                            <div class="intro__project--tech">
+                                <img class="intro__project--short" src="./images/logo/Angular_trimmed.svg" alt="Angular">
+                                <img class="intro__project--long" src="./images/logo/firebaselogo.png" alt="Firebase">
+                                <img class="intro__project--long" src="./images/logo/sass.png" alt="SASS">
+                                <img class="intro__project--short" src="./images/logo/HTML_Logo2.svg" alt="HTML5">
+                            </div>
+                        </div>
+                        <div class="intro__project--back--one">
+                            <div class="intro__project--links">
+                                <a href="https://reportszone.sweeto.co.uk/" class="intro__top--button" target="_blank">
+                                    <img src="./images/logo/liveicon.png" alt="Live Site">
+                                </a>
+                                <a href="https://github.com/a-bunting/reports" class="intro__top--button" target="_blank">
+                                    <img src="./images/ICON/Git-Icon-1788C.png" alt="GIT">
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="intro__project--floater">
+                    <p>ReportZone is a teachers dream, creating useful reports for student improvement in seconds as opposed to hours.</p>
+                </div>
+            </div>
+            <div class="intro__project">
+                <div class="intro__project--inner">
+                    <!-- frnt is for an image, desc and name -->
+                    <div class="intro__project--front">
+                        <div class="intro__project--image">
+                            <img src="./images/physicsLaptop1.jpg" alt="!This>#Phys" class="intro__project--background">
+                            <img src="./images/physicsLogo.png" alt="!This>#Phys Logo" class="intro__project--logo">
+                        </div>
+                    </div>
+                    <!-- back is for tech and links -->
+                    <div class="intro__project--back">
+                        <div class="intro__project--back--one">
+                            <div class="intro__project--tech">
+                                <img class="intro__project--short" src="./images/logo/Angular_trimmed.svg" alt="Angular">
+                                <img class="intro__project--long" src="./images/logo/sass.png" alt="SASS">
+                                <img class="intro__project--short" src="./images/logo/HTML_Logo2.svg" alt="HTML5">
+                            </div>
+                        </div>
+                        <div class="intro__project--back--one">
+                            <div class="intro__project--links">
+                                <a href="https://physics.sweeto.co.uk/" class="intro__top--button" target="_blank">
+                                    <img src="./images/logo/liveicon.png" alt="Live Site">
+                                </a>
+                                <a href="https://github.com/a-bunting/physics-project" class="intro__top--button" target="_blank">
+                                    <img src="./images/ICON/Git-Icon-1788C.png" alt="GIT">
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="intro__project--floater">
+                    <p>!This>#Phys is a suite of simulations designed for student to attain 'lifelike' data, and better understand empirical science.</p>
+                </div>
+            </div>
+            <div class="intro__project">
+                <div class="intro__project--inner">
+                    <!-- frnt is for an image, desc and name -->
+                    <div class="intro__project--front">
+                        <div class="intro__project--image">
+                            <img src="./images/wordleLaptop1.jpg" alt="WordleLeagues" class="intro__project--background">
+                            <img src="./images/wordlelogo.png" alt="WordleLeagues Logo" class="intro__project--logo">
+                        </div>
+                    </div>
+                    <!-- back is for tech and links -->
+                    <div class="intro__project--back">
+                        <div class="intro__project--back--one">
+                            <div class="intro__project--tech">
+                                <img class="intro__project--short" src="./images/logo/Angular_trimmed.svg" alt="Angular">
+                                <img class="intro__project--short" src="./images/logo/nodejs.png" alt="NodeJS">
+                                <img class="intro__project--long" src="./images/logo/sass.png" alt="SASS">
+                                <img class="intro__project--short" src="./images/logo/HTML_Logo2.svg" alt="HTML5">
+                                <img class="intro__project--long" src="./images/logo/mongodb.svg" alt="MongoDB">
+                            </div>
+                        </div>
+                        <div class="intro__project--back--one">
+                            <div class="intro__project--links">
+                                <a href="https://wordleleague.sweeto.co.uk/" class="intro__top--button" target="_blank">
+                                    <img src="./images/logo/liveicon.png" alt="Live Site">
+                                </a>
+                                <a href="https://github.com/a-bunting/wordle-leagues" class="intro__top--button" target="_blank">
+                                    <img src="./images/ICON/Git-Icon-1788C.png" alt="GIT">
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="intro__project--floater">
+                    <p>Designed to work seamlessly across devices, WordleLeagues is the site you goto when its not just fun enough to play Wordle, you need to beat your friends at it too!</p>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</div>
